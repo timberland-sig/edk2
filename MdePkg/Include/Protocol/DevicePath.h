739260f90ac70e010544717e23cef8924509c4ee
Murali Manohar Shanm <Murali_Manohar_Shanm@Dell.com>
2023-03-30 15:26:29 +0200

    MdePkg/DevicePath: Added NVMe-oF Device Path node.
    Definition of this node was introduced in UEFI specification v2.10.
    
    Signed-off-by: Doug Farley <Douglas_Farley@dell.com>
    Co-authored-by: Amit Jain <amit_jain9@dell.com>
    Co-authored-by: Lenny Szubowicz <lszubowi@redhat.com>

--- MdePkg/Include/Protocol/DevicePath.h
+++ MdePkg/Include/Protocol/DevicePath.h
@@ -6,6 +6,7 @@
   it can not contain things like PCI bus numbers that change from boot to boot.
 
 Copyright (c) 2006 - 2018, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2021 - 2022, Dell Technologies. All rights reserved.<BR>
 SPDX-License-Identifier: BSD-2-Clause-Patent
 
 **/
@@ -939,6 +940,20 @@ typedef struct {
 #define ISCSI_LOGIN_OPTION_CHAP_UNI                    0x2000
 
 ///
+/// NVMeOF Device Path SubType
+///
+#define MSG_NVMEOF_DP              0x22
+typedef struct {
+  EFI_DEVICE_PATH_PROTOCOL  Header;
+  UINT8                     Nidt;
+  ///
+  /// Namespace identifier (NSID).
+  ///
+  UINT8                     NamespaceUuid[16];
+  /// CHAR8                 NvmeOf Target Name.
+} NVMEOF_DEVICE_PATH;
+
+///
 /// VLAN Device Path SubType.
 ///
 #define MSG_VLAN_DP  0x14
@@ -1267,6 +1282,7 @@ typedef union {
   ATAPI_DEVICE_PATH                          Atapi;
   SCSI_DEVICE_PATH                           Scsi;
   ISCSI_DEVICE_PATH                          Iscsi;
+  NVMEOF_DEVICE_PATH                         NvmeOf;
   FIBRECHANNEL_DEVICE_PATH                   FibreChannel;
   FIBRECHANNELEX_DEVICE_PATH                 FibreChannelEx;
 
@@ -1323,6 +1339,7 @@ typedef union {
   ATAPI_DEVICE_PATH                          *Atapi;
   SCSI_DEVICE_PATH                           *Scsi;
   ISCSI_DEVICE_PATH                          *Iscsi;
+  NVMEOF_DEVICE_PATH                         *NvmeOf;
   FIBRECHANNEL_DEVICE_PATH                   *FibreChannel;
   FIBRECHANNELEX_DEVICE_PATH                 *FibreChannelEx;
 
