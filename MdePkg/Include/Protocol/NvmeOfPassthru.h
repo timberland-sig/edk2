225c2a1aa5b4c1eb40d601e30ea8539133749f80
Maciej Rabeda <maciej.rabeda@intel.com>
2023-04-03 14:20:30 +0200

    MdePkg: Added EDKII_NVMEOF_PASSTHRU_PROTOCOL definition and GUID.
    This protocol is required (at minimum) by Boot Manager libraries to
    obtain boot description for a handle supported by NVMe-over-Fabrics boot
    driver(s).
    
    Signed-off-by: Doug Farley <Douglas_Farley@dell.com>
    Co-authored-by: Amit Jain <amit_jain9@dell.com>
    Co-authored-by: Hrishikesh Gokhale <Hrishikesh_Gokhale@dell.com>

---
+++ MdePkg/Include/Protocol/NvmeOfPassthru.h
@@ -0,0 +1,46 @@
+/** @file
+  Header for NVMe-oF Passthrough protocol.
+
+  It is meant to be implemented by EDKII NVMe-oF drivers
+  to allow for external control over the driver (e.g. from CLI utilities).
+  It also shall allow boot manager libraries to obtain an NVMe-oF-specific
+  boot description of a given, supported handle.
+
+  Copyright (c) 2023, Intel Corporation. All rights reserved.<BR>
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#ifndef _EDKII_NVMEOF_PASSTHRU_H_
+#define _EDKII_NVMEOF_PASSTHRU_H_
+
+typedef struct _EDKII_NVMEOF_PASSTHRU_PROTOCOL EDKII_NVMEOF_PASSTHRU_PROTOCOL;
+
+/**
+  Obtain NVMe-oF-specific boot description for a given supported handle.
+
+  Caller is responsible for freeing the memory for the description allocated
+  by this function.
+
+  @param[in]  This                Pointer to EDKII_NVMEOF_PASSTHRU_PROTOCOL instance.
+  @param[in]  Handle              Handle for which to obtain boot description.
+  @param[out] Description         Resultant boot description.
+
+  @retval EFI_SUCCESS             Boot description was successfully created.
+  @retval EFI_UNSUPPORTED         Handle is not related to an NVMe-oF namespace.
+**/
+typedef
+EFI_STATUS
+(EFIAPI *EDKII_NVMEOF_PASSTHRU_GET_BOOT_DESC)(
+  IN  EDKII_NVMEOF_PASSTHRU_PROTOCOL   *This,
+  IN  EFI_HANDLE                       Handle,
+  OUT CHAR16                           **Description
+  );
+
+typedef struct _EDKII_NVMEOF_PASSTHRU_PROTOCOL {
+  EDKII_NVMEOF_PASSTHRU_GET_BOOT_DESC    GetBootDesc;
+} EDKII_NVMEOF_PASSTHRU_PROTOCOL;
+
+extern EFI_GUID  gEdkiiNvmeofPassThruProtocolGuid;
+
+#endif /* _EDKII_NVMEOF_PASSTHRU_H_ */
