8a1b4277772ecfd485fb486bd440241328f2fe61
Murali Manohar Shanm <Murali_Manohar_Shanm@Dell.com>
2023-04-04 18:15:11 +0200

    NetworkPkg: Introduce NvmeOfDxe driver.
    This driver allows for connecting to remote NVMe namespaces over TCP
    fabric and expose them as block devices upwards the software stack.
    
    Signed-off-by: Doug Farley <Douglas_Farley@dell.com>
    Co-authored-by: Amit Jain <amit_jain9@dell.com>
    Co-authored-by: Hrishikesh Gokhale <Hrishikesh_Gokhale@dell.com>
    Co-authored-by: Maciej Rabeda <maciej.rabeda@intel.com>
    Co-authored-by: Naoyuki Mori <naoyuki.mori@intel.com>
    Co-authored-by: Martin Wilck <mwilck@suse.com>

---
+++ NetworkPkg/NvmeOfDxe/NvmeOfCliInterface.h
@@ -0,0 +1,17 @@
+/** @file
+  Cli Interface file for NvmeOf driver.
+
+  Copyright (c) 2021 - 2023, Dell Inc. or its subsidiaries. All Rights Reserved.<BR>
+  Copyright (c) 2023, Intel Corporation. All rights reserved.<BR>
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#ifndef _NVMEOF_INTERFACE_CLI_H_
+#define _NVMEOF_INTERFACE_CLI_H_
+
+#include <Protocol/NvmeOfPassthru.h>
+
+extern EDKII_NVMEOF_PASSTHRU_PROTOCOL  gNvmeofPassThroughInstance;
+
+#endif
