1c2657967aba84a1402e4e726348a915e216077f
Trevor Cockrell <trevor_cockrell@dell.com>
2023-06-01 10:27:01 -0500

    NetworkPkg/NvmeOfDxe: NVMe-over-Fabrics configuration forms
    Add support for modification of NVMe-oF connection attempts in the
    NVMe-oF configuration submenu of the HII.
    Update NvmeOfDxe readme with configuration form details.
    
    Signed-off-by: Trevor Cockrell <trevor_cockrell@dell.com>

--- NetworkPkg/NvmeOfDxe/NvmeOfDriver.c
+++ NetworkPkg/NvmeOfDxe/NvmeOfDriver.c
@@ -1855,6 +1855,14 @@ NvmeOfDriverUnload (
     }
   }
 
+  //
+  // Unload the NVMe-oF configuration form.
+  //
+  Status = NvmeOfConfigFormUnload (gNvmeOfIp4DriverBinding.DriverBindingHandle);
+  if (EFI_ERROR (Status)) {
+    goto ON_EXIT;
+  }
+
   if (gNvmeOfControllerNameTable != NULL) {
     Status = FreeUnicodeStringTable (gNvmeOfControllerNameTable);
     if (EFI_ERROR (Status)) {
@@ -2127,7 +2135,26 @@ NvmeOfDriverEntry (
     goto Error2;
   }
 
+  //
+  // Initialize the configuration form of NVMe-oF.
+  //
+  Status = NvmeOfConfigFormInit (gNvmeOfIp4DriverBinding.DriverBindingHandle);
+  if (EFI_ERROR (Status)) {
+    goto Error2;
+  }
+
+  //
+  // Initialize the Global data and assign required configuration values.
+  //
+  Status = NvmeOfInitializeGlobalNvData ();
+  if (EFI_ERROR (Status)) {
+    goto Error3;
+  }
+
   return Status;
+
+Error3:
+  NvmeOfConfigFormUnload (gNvmeOfIp4DriverBinding.DriverBindingHandle);
 
 Error2:
   EfiLibUninstallDriverBindingComponentName2 (
