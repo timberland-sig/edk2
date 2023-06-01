35745d8fb7cc61c1a26a8112d42018ad81ad7647
Trevor cockrell <trevor_cockrell@dell.com>
2023-06-01 10:12:17 -0500

    NetworkPkg/Include: Add GUID gNvmeOfConfigGuid
    GUID gNvmeOfConfigGuid added for NvmeOfDxe HII configuration forms.
    
    Signed-off-by: Trevor cockrell <trevor_cockrell@dell.com>

---
+++ NetworkPkg/Include/Guid/NvmeOfConfigHii.h
@@ -0,0 +1,19 @@
+/** @file
+  GUIDs used as HII FormSet and HII Package list GUID in NvmeOfDxe driver.
+
+Copyright (c) 2023, Dell Technologies All rights reserved
+SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#ifndef __NVMEOF_CONFIG_HII_GUID_H__
+#define __NVMEOF_CONFIG_HII_GUID_H__
+
+#define NVMEOF_CONFIG_GUID \
+  { \
+    0x1509cae7, 0x7044, 0x4392, { 0xa2, 0x47, 0x3f, 0x87, 0x0a, 0x19, 0x71, 0x42 } \
+  }
+
+extern EFI_GUID  gNvmeOfConfigGuid;
+
+#endif
