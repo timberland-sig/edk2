9891566555fa7d26be59d694cf86276ea26d39a8
Amit Jain <amit_jain9@dell.com>
2023-09-13 18:06:28 -0500

    NetworkPkg/NvmeOfDxe: NvmeOfCliInterface integration
    Add and integrate NvmeOfCliInterface changes utilizing NvmeOfPassthru
    protocol.
    
    Signed-off-by: Trevor Cockrell <trevor_cockrell@dell.com>

--- NetworkPkg/NvmeOfDxe/NvmeOfCliInterface.h
+++ NetworkPkg/NvmeOfDxe/NvmeOfCliInterface.h
@@ -12,6 +12,27 @@
 
 #include <Protocol/NvmeOfPassthru.h>
 
+#define NVMEOF_CLI_UUID \
+  { \
+    0xc3ccf809, 0x07ed, 0x4b0c, { 0xbf, 0x99, 0xb4, 0x56, 0xf0, 0x95, 0x4d, 0x6c} \
+  }
+
 extern EDKII_NVMEOF_PASSTHRU_PROTOCOL  gNvmeofPassThroughInstance;
+
+/**
+ Install Cantroller Handler
+
+ @param  NVMEOF_CONNECT_COMMAND     connect data
+
+ @retval EFI_SUCCESS                    All OK
+ @retval EFI_OUT_OF_RESOURCES           Out of Resources
+ @retval EFI_NOT_FOUND                  Resource Not Found
+**/
+
+EFI_STATUS
+EFIAPI
+InstallControllerHandler (
+  IN NVMEOF_CONNECT_COMMAND  *ConnectData
+  );
 
 #endif
